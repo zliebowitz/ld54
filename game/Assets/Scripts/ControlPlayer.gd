e6603ddf37c6752f0e6660f59661eea26bdc6270
Joshua Luoni <josh.luoni@gmail.com>
2023-09-29 22:22:49 -0400

    adding WASD movement and adding rotation to player script.

--- game/Assets/Scripts/ControlPlayer.gd
+++ game/Assets/Scripts/ControlPlayer.gd
@@ -1,21 +1,38 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 300
 
 var velocity = Vector2()
+var rotation_dir = 0
 
 func get_input():
 	velocity = Vector2()
+	var input_pressed = false
+	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		input_pressed = true
+
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		input_pressed = true
+
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		input_pressed = true
+
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		input_pressed = true
+	
+		
 	velocity = velocity.normalized() * speed
+	if input_pressed:
+		rotation_dir = velocity.angle() + PI/2
+		
+	
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	rotation = rotation_dir
