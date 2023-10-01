ccc820f143a08d0bea2a75f1fbd9b0322e0508e4
Chris Williams <tophercwill@gmail.com>
2023-10-01 11:31:41 -0400

    Increased Arena Size

--- game/Assets/Scripts/ScreenArea.gd
+++ game/Assets/Scripts/ScreenArea.gd
@@ -9,7 +9,6 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ScreenCollision.set_polygon($ScreenPolygon.polygon)
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
