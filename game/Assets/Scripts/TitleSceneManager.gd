2470583078853b99c3f92c9dbf69ba810cfee81d
Joshua Luoni <josh.luoni@gmail.com>
2023-10-01 23:20:22 -0400

    better management of sounds.

--- game/Assets/Scripts/TitleSceneManager.gd
+++ game/Assets/Scripts/TitleSceneManager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-export(NodePath) onready var audio_stream = get_node(audio_stream) as AudioStreamPlayer
+export(NodePath) onready var audio_stream
 
 export(PackedScene) var next_scene
 
@@ -11,13 +11,15 @@ export(PackedScene) var next_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	audio_stream = get_node(audio_stream) as AudioStreamPlayer
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(!audio_stream.playing): # end of stream going to next scene
-		get_tree().change_scene_to(next_scene)
+	if(audio_stream):
+		if(!audio_stream.playing): # end of stream going to next scene
+			get_tree().change_scene_to(next_scene)
 		
 
 
